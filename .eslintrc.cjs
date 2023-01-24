ea9d826fb525651ef0f2372cb3795514e7c56af9
ember <1843948009@qq.com>
2023-01-24 22:50:34 +0800

    fix: test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,7 @@ module.exports = {
     'prettier/prettier': 'error',
     'arrow-body-style': 'off',
     'prefer-arrow-callback': 'off',
-    '@typescript-eslint/no-var-requires': 0
+    '@typescript-eslint/no-var-requires': 0,
+    'vue/multi-word-component-names': 'off'
   }
 }
