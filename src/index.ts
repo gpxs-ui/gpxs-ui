8c5930ad8175ea402f1c21d2b3f1b12c6b638bb8
ember <1843948009@qq.com>
2023-01-31 15:30:21 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import JSXButton from "./button/JSXButton";
+
+createApp(JSXButton).mount("#app");
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+// https://vitejs.dev/config/
+
+const rollupOptions = {
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+};
+
+export default defineConfig({
+  plugins: [vue(), vueJsx({})],
+  build: {
+    rollupOptions,
+    minify: false,
+    lib: {
+      entry: "./src/entry.ts",
+      name: "SmartyUI",
+      fileName: "smarty-ui",
+      // 导出模块格式
+      formats: ["es", "umd", "iife"],
+    },
+  },
+});
