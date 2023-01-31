875681b187ec39f719049eec22293072a45255ba
ember <1843948009@qq.com>
2023-02-01 00:00:07 +0800

    chore: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 
 const rollupOptions = {
-  external: ["vue", "vue-router"],
+  external: ['vue', 'vue-router'],
   output: {
     globals: {
-      vue: "Vue",
-    },
-  },
-};
+      vue: 'Vue'
+    }
+  }
+}
 
 export default defineConfig({
   plugins: [vue(), vueJsx({})],
@@ -18,11 +19,22 @@ export default defineConfig({
     rollupOptions,
     minify: false,
     lib: {
-      entry: "./src/entry.ts",
-      name: "SmartyUI",
-      fileName: "smarty-ui",
+      entry: './src/entry.ts',
+      name: 'Gpxs-UI',
+      fileName: 'Gpxs-UI',
       // 导出模块格式
-      formats: ["es", "umd", "iife"],
-    },
+      formats: ['es', 'umd', 'iife']
+    }
   },
-});
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom',
+    // 支持tsx组件，很关键
+    transformMode: {
+      web: [/.[tj]sx$/]
+    }
+  }
+})
