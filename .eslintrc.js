512f399ee81a10f62c635895d500a0bbe7f8d431
ember <1843948009@qq.com>
2023-01-31 23:29:03 +0800

    chore: husky+commitlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true
+  },
+  globals: {
+    ga: true,
+    chrome: true,
+    __DEV__: true
+  },
+  // 解析 .vue 文件
+  parser: 'vue-eslint-parser',
+  extends: [
+    'plugin:json/recommended',
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/prettier'
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser' // 解析 .ts 文件
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'error'
+  }
 }
